af5b1720f0c629accb6adc3deda339af2fcbd5ef
Cenk Kaan KANAR <cenkkaankanar99@hotmail.com>
2020-12-05 19:36:57 +0300

    Project folder created and Dataset Class added

---
+++ project/src/Dataset.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class Dataset {
+
+	      private long datasetId;
+	      private String datasetName;
+	      private long maxLabelsPerInstance;
+	      private ArrayList<Label> labelList = new ArrayList<Label>();
+	      private ArrayList<Instance> instanceList = new ArrayList<Instance>();
+		
+	      
+	     
+	      
+	      
+	    public Dataset(long datasetId, String datasetName, long maxLabelsPerInstance) {
+	
+			this.datasetId = datasetId;
+			this.datasetName = datasetName;
+			this.maxLabelsPerInstance = maxLabelsPerInstance;
+		    this.labelList=  new ArrayList<Label>();
+	        this.instanceList=  new ArrayList<Instance>();
+	    }
+		
+	    
+	    
+	    
+	    public void createLabel(long labelId,String labelText) {
+	    	
+	    	Label label = new Label(labelId,labelText);
+	    	labelList.add(label);
+	    
+	    }
+	    	    
+	    
+	    public void createInstance(long instanceId,String labelName) {
+	    	Instance instance = new Instance(instanceId, labelName);
+	        instanceList.add(instance);
+	    }
+          
+	    
+	    
+	    public long getDatasetId() {
+			return datasetId;
+		}
+		public void setDatasetID(long datasetId) {
+			this.datasetId = datasetId;
+		}
+		public String getDatasetName() {
+			return datasetName;
+		}
+		public void setDatasetName(String datasetName) {
+			this.datasetName = datasetName;
+		}
+		public long getMaxLabelsPerInstance() {
+			return maxLabelsPerInstance;
+		}
+		public void setMaxLabelsPerInstance(long maxLabelsPerInstance) {
+			this.maxLabelsPerInstance = maxLabelsPerInstance;
+		}
+		public ArrayList<Label> getLabelList() {
+			return labelList;
+		}
+		public void setLabelList(ArrayList<Label> labelList) {
+			this.labelList = labelList;
+		}
+		public ArrayList<Instance> getInstanceList() {
+			return instanceList;
+		}
+		public void setInstanceList(ArrayList<Instance> instanceList) {
+			this.instanceList = instanceList;
+		}
+	      
+	      
+	
+	
+	
+	
+	
+	
+	
+	
+}
